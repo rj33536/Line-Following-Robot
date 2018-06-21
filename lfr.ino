7f98d7a88d8d7006c5605403eae237d46e19c86c
rj33536 <31067636+rj33536@users.noreply.github.com>
2018-06-21 16:14:37 +0530

    Add files via upload
    Arduino source file for line following robot

---
+++ lfr.ino
@@ -0,0 +1,127 @@
+#define ls A2
+
+#define rs A5
+
+int rm2=3,rm1=2,lm2=5,lm1=4,v =132  ;
+
+void setup() 
+{
+
+pinMode(lm1,OUTPUT);
+
+pinMode(lm2,OUTPUT);
+
+pinMode(rm1,OUTPUT);
+
+pinMode(rm2,OUTPUT);
+  
+pinMode(A0,OUTPUT);
+
+pinMode(A1,OUTPUT);
+
+pinMode(ls,INPUT);
+
+pinMode(A3,OUTPUT);
+
+pinMode(A4,OUTPUT);
+
+pinMode(rs,INPUT);
+
+pinMode(10,INPUT);
+
+pinMode(9,OUTPUT);
+
+digitalWrite(A1,LOW);
+
+digitalWrite(A0,HIGH);
+
+digitalWrite(A4,LOW);
+
+digitalWrite(A3,HIGH);
+
+Serial.begin(9600);
+
+}
+  
+void loop()
+ {
+
+int left=analogRead(ls);
+
+int right=analogRead(rs);
+
+  if((left<=500)&&(right>=500))
+{
+
+digitalWrite(lm1,HIGH);
+
+digitalWrite(lm2,LOW);
+
+digitalWrite(rm1,LOW);
+
+digitalWrite(rm2,HIGH);
+
+analogWrite(9,v);
+
+analogWrite(10,v);
+
+}
+ 
+else
+ if((left>=500)&&(right<=500))
+
+{
+
+digitalWrite(lm1,LOW);
+
+digitalWrite(lm2,HIGH);
+
+digitalWrite(rm1,HIGH);
+
+digitalWrite(rm2,LOW);
+
+analogWrite(9,v);
+
+analogWrite(10,v);
+
+}
+ 
+ else
+  if((left>=500)&&(right>=500))
+
+{
+
+digitalWrite(lm1,LOW);
+
+digitalWrite(lm2,HIGH);
+
+digitalWrite(rm1,HIGH);
+
+digitalWrite(rm2,LOW);
+
+analogWrite(9,v);
+
+analogWrite(10,v);
+
+}
+
+else
+
+{
+
+digitalWrite(lm1,HIGH);
+
+digitalWrite(lm2,LOW);
+
+digitalWrite(rm1,HIGH);
+
+digitalWrite(rm2,LOW);
+
+analogWrite(9,v);
+
+analogWrite(10,v);
+
+}
+
+}
+
