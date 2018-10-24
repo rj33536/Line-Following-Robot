c00b89727775d1c4c0b5b4bfab96d8c66b2695b3
rj33536 <31067636+rj33536@users.noreply.github.com>
2018-10-24 22:55:00 +0530

    LFR WITHOUT PID ARRAY SENSOR

--- lfr.ino
+++ lfr.ino
@@ -1,125 +1,62 @@
-#define ls A2 //left sensor
-
-#define rs A5 //right sensor
-
-int rm2=3,rm1=2,lm2=5,lm1=4,v =132  ;//motors and velocity 
-
-void setup() 
-{
-//setting pinmodes
+int rm1=6,rm2=7,lm1=4,lm2=5,s1=8,s2=9,s3=10,s4=11,s5=12,vleft=10,vright=11;
+float v1=128,v2=128,pos=2; 
+void setup() {
+  //SETTING PINMODES
+pinMode(s1,INPUT);
+pinMode(s2,INPUT);
+pinMode(s3,INPUT);
+pinMode(s4,INPUT);
+pinMode(s5,INPUT); 
 pinMode(lm1,OUTPUT);
-
 pinMode(lm2,OUTPUT);
-
 pinMode(rm1,OUTPUT);
-
 pinMode(rm2,OUTPUT);
-  
-pinMode(A0,OUTPUT);
 
-pinMode(A1,OUTPUT);
+pinMode(vleft,OUTPUT);
+pinMode(vright,OUTPUT);
 
-pinMode(ls,INPUT);
 
-pinMode(A3,OUTPUT);
-
-pinMode(A4,OUTPUT);
-
-pinMode(rs,INPUT);
-
-pinMode(10,INPUT);
-
-pinMode(9,OUTPUT);
-//powering the infrared proximity sensors
-digitalWrite(A1,LOW);
-
-digitalWrite(A0,HIGH);
-
-digitalWrite(A4,LOW);
-
-digitalWrite(A3,HIGH);
-//starting the serial communication
+analogWrite(vleft,v1);
+analogWrite(vright,v2);
 Serial.begin(9600);
-
 }
-  
-void loop()
- {
-
-int left=analogRead(ls);
-
-int right=analogRead(rs);
-
-  if((left<=500)&&(right>=500))
-{
-
-digitalWrite(lm1,HIGH);
-
-digitalWrite(lm2,LOW);
-
-digitalWrite(rm1,LOW);
-
-digitalWrite(rm2,HIGH);
-
-analogWrite(9,v);
-
-analogWrite(10,v);
-
+void loop(){
+for(int i=0;i<5;i++){
+  if(analogRead(i+6)==true){
+   pos=i;
+   switch(i){
+    case 0:
+      digitalWrite(lm1,LOW);
+      digitalWrite(lm2,HIGH);
+      digitalWrite(rm1,HIGH);
+      digitalWrite(rm2,LOW);
+      break;
+   case 1:
+      digitalWrite(lm1,HIGH);
+      digitalWrite(lm2,LOW);
+      digitalWrite(rm1,HIGH);
+      digitalWrite(rm2,LOW);
+      break;
+   case 2:
+      digitalWrite(lm1,HIGH);
+      digitalWrite(lm2,LOW);
+      digitalWrite(rm1,HIGH);
+      digitalWrite(rm2,LOW);
+      break;
+   case 3:
+      digitalWrite(lm1,HIGH);
+      digitalWrite(lm2,LOW);
+      digitalWrite(rm1,LOW);
+      digitalWrite(rm2,HIGH);
+      break;
+   case 4:
+      digitalWrite(lm1,HIGH);
+      digitalWrite(lm2,LOW);
+      digitalWrite(rm1,LOW);
+      digitalWrite(rm2,HIGH);
+      break;
+   
+   }
+  }
 }
- 
-else
- if((left>=500)&&(right<=500))
-
-{
-
-digitalWrite(lm1,LOW);
-
-digitalWrite(lm2,HIGH);
-
-digitalWrite(rm1,HIGH);
-
-digitalWrite(rm2,LOW);
-
-analogWrite(9,v);
-
-analogWrite(10,v);
-
 }
- 
- else
-  if((left>=500)&&(right>=500))
-
-{
-
-digitalWrite(lm1,LOW);
-
-digitalWrite(lm2,HIGH);
-
-digitalWrite(rm1,HIGH);
-
-digitalWrite(rm2,LOW);
-
-analogWrite(9,v);
-
-analogWrite(10,v);
-
-}
-
-else
-{
-digitalWrite(lm1,HIGH);
-
-digitalWrite(lm2,LOW);
-
-digitalWrite(rm1,HIGH);
-
-digitalWrite(rm2,LOW);
-
-analogWrite(9,v);
-
-analogWrite(10,v);
-
-}
-
-}
-
